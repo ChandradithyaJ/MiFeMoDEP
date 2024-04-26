6ccc3d5a7a265d111fbd68abfee8d01041af76ef
CS21B008 <110806373+CS21B008@users.noreply.github.com>
2024-04-26 13:06:37 +0530

    Update preprocessing_joern.py

--- MiFeMoDEP/SourceCode/preprocessing_joern.py
+++ MiFeMoDEP/SourceCode/preprocessing_joern.py
@@ -6,30 +6,13 @@ path_to_line_random = '.'
 code_files_root = "./Preprocessing_joern_test/preprocess_files_500/"
 graph_files_root = "./Preprocessing_joern_test/preprocess_graphs_500/"
 
-# train_df = pd.read_parquet(f'{path_to_line_random}/train.parquet.gzip')
-# train_df = train_df.reset_index(drop=True)
-
-# train_df['target'] = train_df['lines'].apply(lambda line : 0 if len(line) == 0 else 1)
-
-# train_df_1 = train_df[train_df['target'] == 1].sample(600, random_state=42)
-# train_df_0 = train_df[train_df['target'] == 0].sample(600, random_state=42)
-# print(len(train_df_0))
-# print(len(train_df_1))
-
-# # Combine the DataFrames
-# train_df = pd.concat([train_df_1, train_df_0], ignore_index=True)
-# train_df['target'] = train_df['lines'].apply(lambda line : 0 if len(line) == 0 else 1)
-# content_list = train_df['content']
-# train_df.drop('content',axis=1,inplace=True)
-# train_df.to_csv("random_1200_balanced.csv")
-# train_df['content'] = content_list.apply(lambda x : '' if x is None else x.decode("latin-1"))
-
 train_df = pd.read_csv("balanced_500_test_source_code.csv")
 train_df['content'] = train_df["content"].apply(lambda x : '' if x is None else x)
 train_df["content"] = train_df["content"].apply(lambda x: '' if type(x)==float else x)
 
 print(train_df["content"][327])
 
+# extracting CPG graphs using joern
 def extract_graph_from_file(filename, output_name):
     global graph_files_root
     result = subprocess.run(["joern-parse", filename], capture_output=True)
@@ -37,6 +20,7 @@ def extract_graph_from_file(filename, output_name):
     if result.returncode == 0:
         subprocess.run(["joern-export", "--repr=all", "--out", graph_files_root+output_name]) 
 
+# defectors dataset has code content in dataframe so converting it to a python file for joern to parse
 def write_to_file(row,index):
     filename = str(index)
     content = row['content']
